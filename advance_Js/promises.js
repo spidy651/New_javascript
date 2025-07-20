f149146663e1a7e5d69ff3bcad757eac52db0fb5
Sandeep Mishra <sandeepmsr123@gmail.com>
2025-07-20 22:35:00 +0000

    .

--- advance_Js/promises.js
+++ advance_Js/promises.js
@@ -28,3 +28,51 @@ const PromiseThree = new Promise(function(resolve , reject){
 PromiseThree.then(function(user){
 console.log(user);
 })
+
+const PromiseFour = new Promise(function(resolve , reject){
+   
+    setTimeout(function(){
+         const error = false;
+    if(!error){
+        resolve({username:"sandeep" , email: "sandeepmsr123@gmail.com"});
+    }
+   else{
+    reject("ERROR : something went wrong ");
+   }
+    } , 1000)
+
+})
+
+PromiseFour.then((user)=>{
+    console.log(user);
+    return user.username;   //this return is not going to any user//
+}).then((username)=>{
+    console.log(username);
+     
+}).catch(function(error){
+    console.log(error);
+}).finally(()=>{
+    console.log("the promise has been rejected or resolved");
+})
+
+const PromiseFive = new Promise(function(resolve , reject){
+    setTimeout(function(){
+         const error = true;
+    if(!error){
+        resolve({username:"mohan" , email: "mohanmsr123@gmail.com"});
+    }
+   else{
+    reject("ERROR :  JS went wrong ");
+   }
+    } , 1000) 
+})
+async function ConsumePromiseFive(){
+    try{
+        const response = await PromiseFive;
+        console.log(response);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+ConsumePromiseFive();
