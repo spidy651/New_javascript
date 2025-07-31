6e5aba231a9806e382b1cf80099f4bf35ad9b6b3
Sandeep Mishra <sandeepmsr123@gmail.com>
2025-07-31 20:48:27 +0000

    .

--- advance_Js/staticProp_classes.js
+++ advance_Js/staticProp_classes.js
@@ -16,3 +16,13 @@ const sandeep = new User("sandeep");
 console.log(sandeep.createId());
 
 //if we want that no one can access id then we can use staitc keyword before that and even you extend this class to another class you will still not be able to access it 
+ //example
+
+ class teacher extends User {
+constructor(username , email){
+super(username);
+this.email = email;
+}
+ }
+ const iphone = new teacher("iphone" , "iphone@123");
+ console.log(iphone.createId());   //this will also give error of static keyword is used
