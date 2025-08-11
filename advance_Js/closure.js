7fcd8815ab1af0e87bf41280097df348211e5c8f
Sandeep Mishra <sandeepmsr123@gmail.com>
2025-08-11 09:46:48 +0000

    .

--- advance_Js/closure.js
+++ advance_Js/closure.js
@@ -20,11 +20,25 @@ function outer(){
     }
     function innerTwo(){
         console.log("innerTwo" , username);
-        console.log("middle" , BrotherElement);  //this will also give error as BrotherElement is present in inner
+        //console.log("middle" , BrotherElement);  //this will also give error as BrotherElement is present in inner
     }
     inner()
     innerTwo();
 }
 
 outer()
-console.log("too outer" , username); //this will give error as we are trying to access username outside outer function
+//console.log("too outer" , username); //this will give error as we are trying to access username outside outer function
+
+//lets take an example of closure 
+
+function makeFun(){
+    const name = "morzilla"
+    function DisplayName(){
+ console.log(name);
+    }
+    return DisplayName;    //when we return a function then its not only the function that get return it also return its lexical scope
+}
+
+const MyFunc =  makeFun();
+MyFunc();
+
