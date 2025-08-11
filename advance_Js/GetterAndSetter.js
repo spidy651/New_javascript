523fd8e0baeac7e11b755854fc68976cf371e8f3
Sandeep Mishra <sandeepmsr123@gmail.com>
2025-08-11 08:05:52 +0000

    .

--- advance_Js/GetterAndSetter.js
+++ advance_Js/GetterAndSetter.js
@@ -39,3 +39,19 @@ Object.defineProperty(this , 'address' , {
 }
 const chai = new User2("sandeepmsr","udaipur");
 console.log(chai.username);
+
+//ANOTHER SYNTAX using object
+
+const user3 = {
+_email : 'sn13@',
+_password : "abcd",
+
+get email(){
+    return this._email.toUpperCase();
+},
+set email(value){
+  this._email = value;
+}
+}
+
+console.log(user3.email);
